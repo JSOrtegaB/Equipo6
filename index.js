2d64ce0e018c86d19070867d978be153a349ce88
Juan Ortega <A01794327@tec.mx>
2023-10-27 09:09:54 -0600

    funciona menu

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const axios = require('axios');
-const { data } = require('./datos');
+import axios from 'axios';
+import data from './datos.js';
+import inquirer from 'inquirer';
 
-baseURL = 'https://moneyedwren.backendless.app/api/data/';
+const baseURL = 'https://moneyedwren.backendless.app/api/data/';
 
 const getProducts = async () => {
     const response = await axios.get(`${baseURL}products`);
@@ -33,17 +34,17 @@ const deleteProduct = async (id) => {
 //CRUD   Create, Read, Update, Delete
 
 //crea los registros del archivo datos.js
-/* (async () => {
+const create = async () => {
     console.log("total de datos a grabar: ", data.length);
     const products = await getProducts();
     data.map(async (product, index) => {
         const result = await createProduct(product);
-        console.log(`${index} - producto creado ${product.objectId}  result:${JSON.stringify(result, null, 4)}`);
+        console.log(`${index} - producto creado: ${JSON.stringify(result, null, 4)}`);
     });
-})(); */
+};
 
 //consulta todos los registros
-/* (async () => {
+const getAll = async () => {
     console.log("total de datos a grabar: ", data.length);
     const products = await getProducts();
     products.map((product, index) => {
@@ -51,30 +52,75 @@ const deleteProduct = async (id) => {
     });
     console.log(`Total de registros: ${products.length} `);
 
-})(); */
+};
 
 //actualiza el registro 0 del backendless
-/* (async () => {
+const update = async () => {
     const products = await getProducts();
     const result = await updateProduct(products[0].objectId, { apr: 0.0 });
     console.log(`result:${JSON.stringify(result, null, 4)}`);
 
-})(); */
+};
 
 
 //borra todos los registros
-/* (async () => {
+const deleteAll = async () => {
     const products = await getProducts();
     products.map(async (product, index) => {
         const result = await deleteProduct(product.objectId);
         console.log(`${index} - producto borrado ${product.objectId}  result:${JSON.stringify(result, null, 4)}`);
     });
-})(); */
+};
 
 
 
 
 
+
+
+
+function askForOption() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'option',
+                message: 'Select a function to execute or press x to exit:',
+                choices: [
+                    'Create records from datos.js',
+                    'Query all records',
+                    'Update the first record',
+                    'Delete all records',
+                    'Exit (x)',
+                ],
+            },
+        ])
+        .then(async (answers) => {
+            switch (answers.option) {
+                case 'Create records from datos.js':
+                    await create();
+                    break;
+                case 'Query all records':
+                    await getAll();
+                    break;
+                case 'Update the first record':
+                    await update();
+                    break;
+                case 'Delete all records':
+                    await deleteAll();
+                    break;
+                case 'Exit (x)':
+                    console.log('Exiting...');
+                    process.exit();
+                    break;
+            }
+            // Ask for another option after executing the selected function
+            askForOption();
+        });
+}
+
+// Start by asking for the first option
+askForOption();
 
 
 
